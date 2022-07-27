12f03299ca44786f23fd587ae17a943d000313a1
SsudaLim <ssudalim1@gmail.com>
2022-07-27 22:34:42 +0900

    baekjoon3460

---
+++ baekjoon3460.py
@@ -0,0 +1,41 @@
+MAXBITLEN = 20
+
+#create bit array
+i = 1
+numByLoc = [i]
+for _ in range(MAXBITLEN-1):
+    i = i*2
+    numByLoc.append(i)
+
+def main():
+    nums = getInput()
+    for num in nums:
+        binaryLoc = getBinaryByDecimal(num)
+        printArr(binaryLoc)
+
+def getInput():
+    len = int(input())
+    nums = [ int(input()) for _ in range(len)]
+    return nums
+
+
+def getBinaryByDecimal(decimal):
+    binaryLoc = []
+    for i in range(MAXBITLEN-1, -1, -1):
+        num = numByLoc[i]
+        remain = decimal - num
+        if remain < 0 :
+            continue
+        decimal = remain
+        binaryLoc.append(i)
+
+    binaryLoc.reverse()
+    return binaryLoc
+
+def printArr(arr):
+    for i in arr:
+        print(i, end = ' ')
+    print()
+
+if __name__ == '__main__':
+    main()
